9a79ee3f83157ef17fb5c84de0424b8d4589e87b
rosine <rosinemilie@yahoo.fr>
2023-02-19 15:49:57 +0100

    script modified

--- jenkins-ubuntu.sh
+++ jenkins-ubuntu.sh
@@ -9,9 +9,9 @@
 sudo apt-get update
 ##Install java
 sudo apt-get install openjdk-11-jdk -y
-wget -q -0 -https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -sudo apt-get update
+wget -q -0 -https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -y
+sudo apt-get update
 sudo apt-get install jenkins -y
 sudo apt install git -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
-  
